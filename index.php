4b13dcb6400b8c0099a088830d6d4cccd3cf3968
foxirving <amy.eli.irving@gmail.com>
2017-07-10 12:28:46 -0700

    Footer Added

--- index.php
+++ index.php
@@ -9,15 +9,19 @@
 
 <h1>My first PHP page</h1>
 
-// <?php
-// $PageTitle ="New Page Title";
+<?php
+$PageTitle ="New Page Title";
 
-// function customPageHeader(){ ?>
-<!--   <h1> "HEADER RIGHT HERE" </h1> -->
-// <?php }
+function customPageHeader(){ ?>
+  <h1> "HEADER RIGHT HERE" </h1>
+<?php }
+include_once('header.php');
 
-// include_once('header.php');
-// ?>
+function customPageFooter(){ ?>
+	<h1> "FOOTER RIGHT HERE" </h1>
+<?php }
+include_once('footer.php');
+?>
 
 </body>
 </html> 
