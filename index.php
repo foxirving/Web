dfc455f6ca362625b5d0bf42552c6f73e11a9714
foxirving <amy.eli.irving@gmail.com>
2017-07-10 12:30:34 -0700

    Test

--- index.php
+++ index.php
@@ -13,12 +13,10 @@
 $PageTitle ="New Page Title";
 
 function customPageHeader(){ ?>
-  <h1> "HEADER RIGHT HERE" </h1>
 <?php }
 include_once('header.php');
 
 function customPageFooter(){ ?>
-	<h1> "FOOTER RIGHT HERE" </h1>
 <?php }
 include_once('footer.php');
 ?>
