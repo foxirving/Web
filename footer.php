77b66a4346927d3c472dd3d590274425546c534c
foxirving <amy.eli.irving@gmail.com>
2017-07-10 12:34:27 -0700

    Footer Change

--- footer.php
+++ footer.php
@@ -1,12 +1,6 @@
 <!doctype html>
 <<html>
 <head>
-
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
- <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
-    <!-- Additional tags here -->
-    <?php if (function_exists('customPageFooter')){
-      customPageFooter();
-    }?>
 </head>
 </html>
--- header.php
+++ header.php
@@ -1,11 +1,7 @@
 <!doctype html>
 <<html>
 <head>
-
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
- 
- <title><?= isset($PageTitle) ? $PageTitle : "Default Title"?></title>
-    
 </head>
 
 <body>
