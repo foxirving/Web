0236edf83b04fc1b4335601b8583d29204ab1bf1
foxirving <amy.eli.irving@gmail.com>
2017-07-25 14:23:53 -0700

    Automated Push

--- src/pages/home.php
+++ src/pages/home.php
@@ -9,9 +9,8 @@
 
 <p>Home, Center</p>
 
-<img src="../images/MyPhoto.jpg" width="280" height="125" title="" alt="My Photo" />
-
 <?php
+echo "<img src='../images/MyPhoto.jpg'>";
 $homepage = file_get_contents('text/AboutMe.txt');
 echo $homepage;
 ?>
