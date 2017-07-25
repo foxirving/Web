ccab5df4e869f80a75e735176dccb60d91fcc5ba
foxirving <amy.eli.irving@gmail.com>
2017-07-25 13:23:20 -0700

    Automated Push

--- src/header.php
+++ src/header.php
@@ -5,14 +5,15 @@
 
 <body>
 
+
+<div id="header">
 <h1> Header </h1>
-
-
 <a href="index.php?page=home">Home</a>
 <a href="index.php?page=resume">Resume</a>
 <a href="index.php?page=repositories">Repositories</a>
 <a href="index.php?page=gallery">Gallery</a>
 <a href="index.php?page=contact">Contact</a>
+</div>
 
 </body>
 
