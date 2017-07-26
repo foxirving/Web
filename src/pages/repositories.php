67f3b589552247db2038fa57bf0a6cdf904dc371
foxirving <amy.eli.irving@gmail.com>
2017-07-25 20:10:04 -0700

    Automated Push

--- src/pages/repositories.php
+++ src/pages/repositories.php
@@ -18,8 +18,7 @@ echo "<p>{$files[2]}</p>";
 $data = file_get_contents($dir . $files[2]);
 echo $data;
 
-$similarity = 75;
-echo "<div class=\"repository-div\" style=\"width: <?php echo $similarity; ?>\%;\"> <\div>";
+echo "<div class=\"repository-div\"> <\div>";
 ?>
 
 
