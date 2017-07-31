ccfab3ac7b8d6a310e0e60bd48df9d378848facf
foxirving <amy.eli.irving@gmail.com>
2017-07-31 00:41:34 -0700

    Automated Push

--- src/pages/gallery.php
+++ src/pages/gallery.php
@@ -11,6 +11,28 @@
 
 <img src="gallery/art.jpg" alt="Smiley face" height="42" width="42"/>
 
+<?php
+$dir = "gallery/";
+
+// Open a directory, and read its contents
+if (is_dir ( $dir )) {
+	if ($dh = opendir ( $dir )) {
+		while ( ($file = readdir ( $dh )) !== false ) {
+			if ($file != "." && $file != "..") {
+				?>
+     	 <img src=
+       		<?php
+       			echo $dir . $file;
+				?>
+   		 />
+    	<?php
+			}
+		}
+		closedir ( $dh );
+	}
+}
+?>
+
 </div>
 </body>
 
